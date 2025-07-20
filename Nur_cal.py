178f79740b2b33d02c7232028da1c90c9f11b231
Angell6991 <miguel9196.guzman@gmail.com>
2025-07-20 15:58:13 -0500

    add tabulate python

--- Nur_cal.py
+++ Nur_cal.py
@@ -5,11 +5,10 @@ print("Calculo de probabilidad para esquivar en base al daño")
 damage_init     =   int(input("Daño: "))
 dodge_init      =   int(input("Esquivar: "))
 table_result    =   dad.table_damge_dodge(damage_init, dodge_init, "modules/probability/Probabilidad_3D6.dat")
-print(" ")
 
 ###-------------------write_result-------------------------###
-# with    open("Tabla_result.md", "w")   as  f:
-#     f.write(table_result.to_markdown(index=False))
+with    open("Tabla_result.md", "w")   as  f:
+    f.write("Daño: " + str(damage_init) + "\n" + "\n" + "Esquivar: " + str(dodge_init) + "\n" + "\n")
+    f.write(table_result.to_markdown(index=False))
 
-print(table_result)
 
